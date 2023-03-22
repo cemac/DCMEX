c83a21fe8fd637844d210e7bf2461bedc0aee7e4
Helen Burns <h.l.burns@leeds.ac.uk>
2023-03-22 16:56:44 +0000

    :sparkles: pitch correcting success with new method plus numbers for dec's failed method, start of svg diagram to illustrate new method

--- manual/knownobjectsheightestimator_pitchcorrect.py
+++ manual/knownobjectsheightestimator_pitchcorrect.py
@@ -37,12 +37,13 @@ SH = 24
 camera_height = 1.45
 # Height = (Distance to Object x Object height on sensor) / Focal Length
 
+FOV = 27
+
 # equations from: https://www.scantips.com/lights/fieldofviewmath.html might need to find a text book referece!
 
 
 def find_height(P, D, F, SH):
     OHS = find_OHS(P, SH)
-    # Height = (Distance to Object x Object height on sensor) / Focal Length
     H = (D * 10**3 * OHS) / F
     return round(H / 10**3, 2)
 
@@ -53,32 +54,32 @@ def find_height(P, D, F, SH):
 
 def find_OHS(P, SH):
     SHP = 4188
-    # Object height on sensor =  (Sensor height (mm) × Object height (pixels)) / Sensor height (pixels)
     OHS = (SH * P / SHP)
     return OHS
 
+def pitch_correct(P, FOV, h):
+    # See diagram for agles a, b and c
+    a = 90 - P - FOV/2
+    b = 180 - 90 - FOV/2
+    # image plane incline from vertical
+    c = 180 - a - b
+    # x is true height, h is height on incled place
+    x = h * math.cos(math.radians(c))
+    return x
 
-def find_tc(P):
-    # Field of View
-    FOV = 27
-    # theta_cld_from_ceter = 0.5 * FOV / num pixels from center
-    theta_c = 0.5 * FOV / (P - (4160/2) )
-    return(theta_c)
 
 # Read in distance in km
 df = pd.read_csv('pixel_data/known_objects.csv')
 df2 = pd.DataFrame(columns=['Time', 'distance', 'est_height','pitch_corrected', 'actual_height'])
 for i in range(2):
-
-    h = find_height(df["pixel_height"][i], (df["GD_min"][i]) * 1000,
+    # fight height on inclinded image plane
+    h = find_height(df["pixel_height"][i], (df["GD_min"][i])*1000,
                      F, SH)
-    theta_c = df["pitch"][i] + find_tc(h)
-    # h_pitch_corrected = d * tan (theta_c) + cam height
-    # This doesn't work - I think because with a pitched camera the image height is incline
-    # the next script pitch correct uses this 
-    h_pc = df["GD_min"][i] *1000 * math.tan(math.radians(theta_c)) + camera_height * 1000
+    # correct for pitch
+    h_pc = pitch_correct(df["pitch"][i], FOV, h)
 
-    df2.loc[i] = [df["object"][i],df["GD_max"][i],h + camera_height * 1000,h_pc,df["actual_height"][i]]
+    df2.loc[i] = [df["object"][i],df["GD_max"][i],h + camera_height * 1000,
+                  h_pc + camera_height * 1000, df["actual_height"][i]]
 
 
-df2.to_csv('results/known_objects_height.csv')
+df2.to_csv('results/known_objects_pitch_corrected_success.csv')
